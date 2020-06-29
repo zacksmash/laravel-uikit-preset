54e249cf825cb2a8a656a4d67664faf9f0364a1a
zacksmash <zachary.warren@me.com>
2020-06-29 10:51:22 -0600

    Update namespace

--- src/UIkitPreset.php
+++ src/UIkitPreset.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LaravelFrontendPresets\UIkitPreset;
+namespace Zacksmash\UIkitPreset;
 
 use Illuminate\Support\Str;
 use Illuminate\Container\Container;
--- src/UIkitPresetServiceProvider.php
+++ src/UIkitPresetServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LaravelFrontendPresets\UIkitPreset;
+namespace Zacksmash\UIkitPreset;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
